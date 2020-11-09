575a9017097f6600205c03f2ed88998588f0122b
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-09 10:27:01 -0600

    -

--- settings/commands/server_start.sh
+++ settings/commands/server_start.sh
@@ -1,8 +1,7 @@
 # spin up mongo in the background
-sh ./settings/commands/db_start.sh &
+sh ./settings/commands/db_start.sh &>./log.nosync.txt  &
 # install all the needed libs for the express server
 npm install
 # keep the express server running and tell it to watch for updates
-npx nodemon main.js &
-# allow the user to control the system
-bash
+npx nodemon main.js &>./log.nosync.txt  &
+
