81cc9cf3d9d41dcdbb9eba45dd477039a82e6441
jeff-hykin <jeff.hykin@gmail.com>
2020-11-09 10:28:13 -0600

    improve restore tool

--- settings/commands/db_restore.sh
+++ settings/commands/db_restore.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash 
+echo "============================"
 echo "Enter the name of the backup"
+echo "============================"
+ls -1 ./backups.nosync
+
 read backup_name
 mongorestore --dir "./backups.nosync/$backup_name"
