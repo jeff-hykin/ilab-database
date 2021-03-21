b5cd22225da963ed62a23bc7d1a47a8226946435
Jeff Hykin <jeff.hykin@gmail.com>
2021-03-21 15:40:52 -0500

    fix changeDb

--- source/interface/changeDb.js
+++ source/interface/changeDb.js
@@ -2,12 +2,5 @@ const { connectToMongoDb } = require("../ezMongoDb/mongoSystem")
 let package = require("../../package.json")
 
 module.exports = async ([databaseName]) => {
-    let result
-    try {
-        result = await connectToMongoDb({...package.parameters.databaseSetup, databaseName })
-    } catch (error) {
-        throw error
-    }
-    // just redo the connect
-    return result
+    await connectToMongoDb({...package.parameters.databaseSetup, databaseName })
 }
