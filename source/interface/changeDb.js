f9a163d6f213b40408f2a48578bbc97376028588
Jeff Hykin <jeff.hykin@gmail.com>
2021-03-21 15:36:53 -0500

    try to add error handling

--- source/interface/changeDb.js
+++ source/interface/changeDb.js
@@ -2,6 +2,12 @@ const { connectToMongoDb } = require("../ezMongoDb/mongoSystem")
 let package = require("../../package.json")
 
 module.exports = async ([databaseName]) => {
+    let result
+    try {
+        result = await connectToMongoDb({...package.parameters.databaseSetup, databaseName })
+    } catch (error) {
+        throw error
+    }
     // just redo the connect
-    return connectToMongoDb({...package.parameters.databaseSetup, databaseName })
+    return result
 }
